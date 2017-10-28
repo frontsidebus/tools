3ed50ae76f3bcdb065e9b1b394f7c803d95a7a1c
Paul B <paulebryant@gmail.com>
2017-10-27 21:42:26 -0500

    Update fstab_mod.sh
    added comment and one-liner version

--- fstab_mod.sh
+++ fstab_mod.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#run this as root
+
+#run as one-liner:
+#backup_fstab () { BACKUP_DIR=/.armor/BACKUP; FSTAB=/etc/fstab; if [ -d $BACKUP_DIR ]; then cp -a $FSTAB $BACKUP_DIR; else cp -a $FSTAB /root/.; fi; }; running_tmp () { TMPFS=("tmpfs""/tmp" "/var/tmp"); for FS in "${TMPFS[@]}"; do mount -o remount,rw,exec $TMPFS; done; }; mod_fstab () { FSTAB=/etc/fstab; sed -i '/.*tmp.*/s/noexec/exec/g' $FSTAB; sed -i '/.*tmp.*/s/defaults/rw\,exec/g' $FSTAB; mount -a; }; backup_fstab; running_tmp; mod_fstab
+
 #LOG=/root/test.log
 #so we have an extra fstab file to rollback to
 
