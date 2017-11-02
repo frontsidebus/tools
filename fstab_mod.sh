410ca47e6bbc92fd93b3992be957eb8a183880e1
Paul B <paulebryant@gmail.com>
2017-11-02 03:55:08 -0500

    Update fstab_mod.sh
    fixed issue with only tmpfs being mounted three times.
    fixed issue where non-existent tmp filesystems were erroring straight to standard out

--- fstab_mod.sh
+++ fstab_mod.sh
@@ -3,10 +3,9 @@
 #run this as root
 
 #run as one-liner:
-#backup_fstab () { BACKUP_DIR=/.armor/BACKUP; FSTAB=/etc/fstab; if [ -d $BACKUP_DIR ]; then cp -a $FSTAB $BACKUP_DIR; else cp -a $FSTAB /root/.; fi; }; running_tmp () { TMPFS=("tmpfs""/tmp" "/var/tmp"); for FS in "${TMPFS[@]}"; do mount -o remount,rw,exec $TMPFS; done; }; mod_fstab () { FSTAB=/etc/fstab; sed -i '/.*tmp.*/s/noexec/exec/g' $FSTAB; sed -i '/.*tmp.*/s/defaults/rw\,exec/g' $FSTAB; mount -a; }; backup_fstab; running_tmp; mod_fstab
+#backup_fstab () { BACKUP_DIR=/.armor/BACKUP; FSTAB=/etc/fstab; if [ -d $BACKUP_DIR ]; then cp -a $FSTAB $BACKUP_DIR; else cp -a $FSTAB /root/.; fi; }; running_tmp () { TMPFS=("tmpfs""/tmp" "/var/tmp"); for FS in "${TMPFS[@]}"; do mount -o remount,rw,exec $FS; done; }; mod_fstab () { FSTAB=/etc/fstab; sed -i '/.*tmp.*/s/noexec/exec/g' $FSTAB; sed -i '/.*tmp.*/s/defaults/rw\,exec/g' $FSTAB; mount -a; }; backup_fstab; running_tmp; mod_fstab
 
-#LOG=/root/test.log
-#so we have an extra fstab file to rollback to
+#so we have an fstab file to rollback to
 
 backup_fstab () {
         BACKUP_DIR=/.armor/BACKUP
@@ -28,7 +27,7 @@ running_tmp () {
     )
         for FS in "${TMPFS[@]}"
                 do
-                mount -o remount,rw,exec $TMPFS
+                mount -o remount,rw,exec $FS 2&> /devnull
                 done
 }
 
